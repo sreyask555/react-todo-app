ff71cd50567b3104340a2c095548e930c72c1220
Sreyas <sreyask555@gmail.com>
2020-07-25 22:43:27 +0530

    Updates

---
+++ precache-manifest.4316edd39de90b94232ed105d9c08702.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63629e3884c6edf8803d08ea0c9f71ad",
+    "url": "/react-todo-app/index.html"
+  },
+  {
+    "revision": "236bda9d61385b1e0a8f",
+    "url": "/react-todo-app/static/css/main.578acc8a.chunk.css"
+  },
+  {
+    "revision": "5b155b0de28cb258f0fd",
+    "url": "/react-todo-app/static/js/2.c38ef810.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-todo-app/static/js/2.c38ef810.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "236bda9d61385b1e0a8f",
+    "url": "/react-todo-app/static/js/main.2c34ea8f.chunk.js"
+  },
+  {
+    "revision": "a02306a5582400e4c981",
+    "url": "/react-todo-app/static/js/runtime-main.15b7a15e.js"
+  }
+]);
